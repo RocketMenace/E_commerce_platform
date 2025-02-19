cfb1984ab61328a39e037019fbdfb8239fbd6d3d
RocketMenace <grizzlygit777@gmail.com>
2025-02-19 19:08:51 +0300

    Implementing django filters by country.

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django_countries",
     "rest_framework_simplejwt",
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
--- retail_network/views.py
+++ retail_network/views.py
@@ -1,12 +1,16 @@
 from rest_framework import status
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
 from retail_network.serializers import (NetworkNodeInputSerializer,
                                         NetworkNodeOutputSerializer)
 from retail_network.services import (create_node, delete_node, get_node,
                                      update_node)
+
+from retail_network.models import NetworkNode
 from users.permissions import IsActive
 
 # Create your views here.
@@ -48,3 +52,10 @@ class NetworkNodeUpdateAPIView(APIView):
         node = update_node(node_id, serializer.validated_data)
         output_serializer = NetworkNodeOutputSerializer(node)
         return Response(data=output_serializer.data, status=status.HTTP_200_OK)
+
+class NetworkNodeListAPIView(ListAPIView):
+    permission_classes = [IsAuthenticated & IsActive]
+    queryset = NetworkNode.objects.all().prefetch_related("contacts")
+    serializer_class = NetworkNodeOutputSerializer
+    filter_backends = [SearchFilter, ]
+    search_fields = ["contacts__country"]
